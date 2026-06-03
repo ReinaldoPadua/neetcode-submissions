5f7317c50861abd2639f1abfb42e7d8a5c35b26a
Reinaldo Padua <reinaldo.padua@unoesc.edu.br>
2026-06-02 22:16:13 -0300

    Add: anagram-groups - submission-4

---
+++ Data Structures & Algorithms/anagram-groups/submission-4.java
@@ -0,0 +1,49 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+
+        Map<String, String> anagrams = new HashMap<String,String>();
+        List<List<String>> anagramsreturn = new ArrayList<>();
+        
+        for(int i=0;i<strs.length;i++){
+            String a = strs[i];
+            String tmpA = anagrams.get(a);
+            if(tmpA==null){
+                List<String> subAnagrams = new ArrayList<>();
+                subAnagrams.add(a);
+                anagrams.put(a,a);
+
+                for(int j=0;j<strs.length;j++){
+                    String b = strs[j];
+
+                    if(i!=j) {
+                        if(isAnagram(a,b)){
+                            if(i!=j){
+                                subAnagrams.add(b);
+                                anagrams.put(b,b);
+                            }
+                        }
+                    }
+                }
+                anagramsreturn.add(subAnagrams);
+            }
+        }
+        return anagramsreturn;
+    }
+
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length())
+            return false;
+        
+        int[] counts = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            counts[s.charAt(i) - 'a']++;
+            counts[t.charAt(i) - 'a']--;
+        }
+        
+        for (int count : counts) {
+            if (count != 0) return false;
+        }
+        
+        return true;
+    }
+}
